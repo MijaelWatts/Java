8d80d7fa36dd82a370a9d3c8fce5990f08d360d5
Victor Lopez (UST <U34006@ust-global.com>
2018-01-31 16:24:05 -0600

    File updated for working in different IDEs

--- Core/Collections/src/com/watts/Main.java
+++ Core/Collections/src/com/watts/Main.java
@@ -1,97 +1,22 @@
 package com.watts;
 
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        Main main = new Main();
+    	TheSetInterface setCollection = new TheSetInterface();
+    	TheListInterface listCollection = new TheListInterface();
 
-        System.out.println("\n☺☺☺☺☺☺☺☺☺☺☺☺ THE SET INTERFACE ☺☺☺☺☺☺☺☺☺☺☺☺");
-        System.out.println("\n→→→→→→→→→→ Set Interface Basics ←←←←←←←←←←\n");
-        main.setInterfaceBasics();
-        System.out.println("\n→→→→→→→→→→ Set Interface Bulks ←←←←←←←←←←\n");
-        main.setInterfaceBulks();
+        System.out.println("\t>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> THE SET INTERFACE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        System.out.println("\n \t------- Set Interface Basics -------");
+        setCollection.setInterfaceBasics();
+        System.out.println("\n \t------- Set Interface Bulks -------");
+        setCollection.setInterfaceBulks();
+        
+        System.out.println("\n \t>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> THE LIST INTERFACE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+        System.out.println("\n \t------- List Interface Basics -------");
+        listCollection.listInterfaceBasics();
+        System.out.println("\n \t ------- List Interface Iterator -------");
+        listCollection.listInterfaceIterator();
     }
-
-    /**
-     * Method explaining with an example how the Set interface works.
-     */
-    private void setInterfaceBasics(){
-        int index = 0;
-        List<String> phoneContacts = new ArrayList<>();
-
-        phoneContacts.add("Lanie Watts");
-        phoneContacts.add("Eliette Watts");
-        phoneContacts.add("Mijael Watts");
-        phoneContacts.add("Mijael Watts");
-
-        Set<String> contactsHashSet = new HashSet<>(phoneContacts);
-        Set<String> contactsTreeSet = new TreeSet<>(phoneContacts);
-        Set<String> contactsLinkedHashSet = new LinkedHashSet<>(phoneContacts);
-
-        String[] message = {
-                "\nRepeated contacts removed (HashSet) order changes due to hash keys generated:",
-                "\nRepeated contacts removed (TreeSet) ascending order:",
-                "\nRepeated contacts removed (LinkedHashSet) insertion order respected:"
-        };
-        Set[] setArray = {contactsHashSet, contactsTreeSet, contactsLinkedHashSet};
-
-        System.out.println("Original contacts (ArrayList):");
-        for(String contactName : phoneContacts) {
-            System.out.println(contactName);
-        }
-
-        while(index < 3){
-            Set<String> setString = setArray[index];
-
-            System.out.println(message[index]);
-
-            for(String contactName : setString) {
-                System.out.println(contactName);
-            }
-
-            index++;
-        }
-    }
-
-    /**
-     * Method explaining with an example how the Set interface Bulks work.
-     */
-    private void setInterfaceBulks() {
-        Set<String> farmA = new LinkedHashSet<>();
-        Set<String> farmB = new LinkedHashSet<>();
-
-        farmA.add("Cows");
-        farmA.add("Horses");
-        farmA.add("Pigs");
-
-        farmB.add("Chickens");
-        farmB.add("Horses");
-        farmB.add("Sheeps");
-
-        System.out.println("Animals in Farm A: " + farmA);
-        System.out.println("Animals in Farm B: " + farmB);
-
-        // containsAll() returns true if s2 is a subset of s1.
-        Set<String> container = new LinkedHashSet<>(farmA);
-        boolean isASubSet = container.containsAll(farmB);
-
-        // addAll() transforms s1 into the union of s1 and s2.
-        Set<String> union = new LinkedHashSet<>(farmA);
-        union.addAll(farmB);
-
-        // retainAll() transforms s1 into the intersection of s1 and s2 (elements common to both sets).
-        Set<String> intersection = new LinkedHashSet<>(farmA);
-        intersection.retainAll(farmB);
-
-        // removeAll() transforms s1 into the set difference of s1 and s2.
-        Set<String> difference = new LinkedHashSet<>(farmA);
-        difference.removeAll(farmB);
-
-        System.out.println("\nFarmA contains the same animals of FarmB? " + isASubSet + (isASubSet ? container : ""));
-        System.out.println("Union of FarmA and FarmB: " + union);
-        System.out.println("Animals repeated in FarmA and FarmB: " + intersection);
-        System.out.println("Animals in FarmA that exist not in FarmB:" + difference);
-    }
+    
 }
