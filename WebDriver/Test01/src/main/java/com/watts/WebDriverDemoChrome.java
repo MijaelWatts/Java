02d80ca981057c43aa5f165534188553ea09b4ea
Victor Lopez (UST <U34006@ust-global.com>
2018-02-22 15:09:30 -0600

    Subject updated.

--- WebDriver/Test01/src/main/java/com/watts/WebDriverDemoChrome.java
+++ WebDriver/Test01/src/main/java/com/watts/WebDriverDemoChrome.java
@@ -32,7 +32,7 @@ public class WebDriverDemoChrome {
         
         driver.findElement(By.xpath("//*[@id=\"primaryContainer\"]/div[4]/div/div[1]/div/div[4]/div[3]/div/div[5]/div[1]/div/div[3]/div[4]/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[2]/div[1]/div/div/div/span/div[1]/form/input")).sendKeys("victor.lopez@ust-global.com");
         
-        driver.findElement(By.xpath("//*[@id=\"primaryContainer\"]/div[4]/div/div[1]/div/div[4]/div[3]/div/div[5]/div[1]/div/div[3]/div[4]/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[6]/div[2]/div/input")).sendKeys("Automation with web driver");
+        driver.findElement(By.xpath("//*[@id=\"primaryContainer\"]/div[4]/div/div[1]/div/div[4]/div[3]/div/div[5]/div[1]/div/div[3]/div[4]/div/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[6]/div[2]/div/input")).sendKeys("Automation with Web Driver Demo1");
 
         driver.findElement(By.xpath("//*[@id=\"primaryContainer\"]/div[4]/div/div[1]/div/div[4]/div[3]/div/div[5]/div[1]/div/div[3]/div[4]/div/div[1]/div[2]/div[2]/div[2]/div[3]/div/div[3]/div[1]/div[3]/div")).sendKeys("This message was written using selenium web driver with Java.");
 
